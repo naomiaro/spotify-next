070800bc45404fd76fdb15f5f51ea6b0a207ddfc
Naomi Aro <naomiaro@gmail.com>
2022-04-09 10:27:42 -0700

    Add next auth spotify

---
+++ lib/api/spotify.ts
@@ -0,0 +1,9 @@
+import SpotifyWebApi from "spotify-web-api-node";
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.SPOTIFY_CLIENT_ID,
+  clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+  redirectUri: process.env.SPOTIFY_REDIRECT_URI,
+});
+
+export default spotifyApi;
